3ac44df52b48821aec7ea2aac17d0188043564ea
GuLABatUW <92705315+GuLABatUW@users.noreply.github.com>
2021-11-19 14:11:09 -0800

    Delete generate_matrix.R

--- Vseg_singleCellAnalysis/generate_matrix.R
+++
@@ -1,4 +0,0 @@
-generate_matrix <- function(feature, region, algorithm, final_file, cut, work_dir){
-	tmp_cmd = paste("perl ~/ShapeSegVnet/R/shapeImageSegVnet2.pl -e", feature, "-r",region, "-a",algorithm, "-f", final_file, "-c", cut, "-w", work_dir)
-	system(tmp_cmd)
-}
