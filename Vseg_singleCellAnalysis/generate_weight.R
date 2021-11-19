c6be2ecea868fe146a55bac7c31e40904322d4bb
GuLABatUW <92705315+GuLABatUW@users.noreply.github.com>
2021-11-19 14:11:33 -0800

    Delete generate_weight.R

--- Vseg_singleCellAnalysis/generate_weight.R
+++
@@ -1,4 +0,0 @@
-generate_weight <- function(distance_file, coord_file, final_file, out_file){
-	tmp_cmd = paste("perl ~/ShapeSegVnet/R/edgeWeight.pl -d", distance_file, "-c",coord_file, "-f", final_file, "-o", out_file)
-	system(tmp_cmd)
-}
